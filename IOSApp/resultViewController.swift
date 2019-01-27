97790035cd426939a164a1d2f0dd63e00e3a53e2
Andrea Santos <dreasalazarsantos@gmail.com>
2019-01-26 20:45:48 -0800

    added a new view controller

---
+++ IOSApp/resultViewController.swift
@@ -0,0 +1,32 @@
+//
+//  resultViewController.swift
+//  IOSApp
+//
+//  Created by Andrea on 1/26/19.
+//  Copyright © 2019 AndreaKayla. All rights reserved.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+ 
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
